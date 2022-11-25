17a6fb1560f090180e3db0ffedece61b3682b03c
James Buckley <james.buckley@polestar-eam.com>
2022-11-25 20:47:26 +0000

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by James Buckley on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
